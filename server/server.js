668ab11284d01a3a8acf30f0ff60360c38b46550
Nishant Boro <nishant.boro@practo.com>
2021-04-05 15:01:14 +0530

    package.json script modified

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const request = require("request");
 
 const app = express();
 const publicpath = path.join(__dirname, "..", "dist");
@@ -9,7 +10,18 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicpath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicpath, "index.html"));
+  if (req.path === "/search-movie/") {
+    request(
+      `http://www.omdbapi.com/?apikey=94c39b02&s=${req.query.title}&type=movie`,
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          res.send(response.body);
+        }
+      }
+    );
+  } else {
+    res.sendFile(path.join(publicpath, "index.html"));
+  }
 });
 
 app.listen(port, () => {
